1e9dd1d5448af32da3429b9841caaec6939c35c2
sahibgaba1 <sahib.gaba@ryerson.ca>
2022-05-15 19:49:08 -0400

    formatting

--- server.js
+++ server.js
@@ -47,7 +47,9 @@ app.post('/api/generate', async (req, res) => {
     temperature: 0.6,
     max_tokens: 20,
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+  console.log(completion)
+  haiku = completion.data.choices[0].text
+  res.status(200).json({ result: haiku });
 });
 
 app.get('/home/me', (req, res) => {
